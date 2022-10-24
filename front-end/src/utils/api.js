5960bccb56264f660956dea2f4f8befd24b8d3ec
Laura Furman <lcfurman11@gmail.com>
2022-10-24 05:05:20 -0400

    "frontend updates"

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,8 +5,8 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL = "https://furman-backend.herokuapp.com";
+// process.env.REACT_APP_API_BASE_URL || "https://furman-backend.herokuapp.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
